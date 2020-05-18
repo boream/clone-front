9220cc6ede17e57f8c81deb0a8967811a7f5b6da
Claudia <claudiabdm@hotmail.es>
2020-05-18 13:09:09 +0200

    Add routes and router outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { UploadComponent } from './components/upload/upload.component';
+import { SearchComponent } from './components/search/search.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'login', component: LoginComponent },
+  // { path: 'signup', component: SignUpComponent },
+  {
+    path: 'home',
+    component: HomeComponent,
+    children:
+      [
+        { path: 'profile', component: ProfileComponent },
+        { path: 'upload', component: UploadComponent },
+        { path: 'search', component: SearchComponent },
+      ]
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
