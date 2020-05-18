b8c3c73653a7b8c6c4f1ae73dc361515e40ffb67
Claudia <claudiabdm@hotmail.es>
2020-05-18 13:06:34 +0200

    Update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { UploadComponent } from './components/upload/upload.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    UploadComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
