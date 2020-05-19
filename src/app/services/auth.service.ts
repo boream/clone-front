00c9eb73f73e1a1ef53c0849926844db39cd3619
Claudia <claudiabdm@hotmail.es>
2020-05-19 11:35:33 +0200

    Add login method

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,19 +1,30 @@
 import { Injectable, Inject } from '@angular/core';
 import { StorageService, LOCAL_STORAGE } from 'ngx-webstorage-service';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup } from '@angular/forms';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  url: string = `${environment.apiUrl}auth`;
+
   constructor(
     @Inject(LOCAL_STORAGE)
     private storage: StorageService,
-    private router: Router
+    private router: Router,
+    private http: HttpClient
   ) { }
 
-  logout(){
+  login(form: FormGroup): Observable<Object> {
+    return this.http.post(`${this.url}login`, form);
+  }
+
+  logout(): void {
     this.storage.clear();
     this.router.navigate(['/login']);
   }
