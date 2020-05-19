fef7cc368fd02c9bee0e8e062fd4591441f96323
Claudia <claudiabdm@hotmail.es>
2020-05-19 09:50:24 +0200

    Modify app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,23 +4,33 @@ import { HomeComponent } from './components/home/home.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { UploadComponent } from './components/upload/upload.component';
 import { SearchComponent } from './components/search/search.component';
+import { AuthGuard } from './guards/auth.guard';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
-  // { path: 'login', component: LoginComponent },
-  // { path: 'signup', component: SignUpComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
   {
     path: 'home',
     component: HomeComponent,
-    children:
-      [
-        { path: 'profile', component: ProfileComponent },
-        { path: 'upload', component: UploadComponent },
-        { path: 'search', component: SearchComponent },
-      ]
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: '',
+        canActivateChild: [AuthGuard],
+        children: [
+          { path: 'profile', component: ProfileComponent },
+          { path: 'upload', component: UploadComponent },
+          { path: 'search', component: SearchComponent },
+        ]
+      }
+    ]
   },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  // { path: '**', component: PageNotFoundComponent }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
