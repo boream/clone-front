90b42061dcd5496872e86d367f8f4e7492858bd8
Claudia <claudiabdm@hotmail.es>
2020-05-20 12:29:38 +0200

    Add auth.interceptor tests

--- src/app/interceptors/auth.interceptor.spec.ts
+++ src/app/interceptors/auth.interceptor.spec.ts
@@ -1,16 +1,79 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, fakeAsync } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { AuthInterceptor } from './auth.interceptor';
+import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
+import { AuthService } from '../services/auth.service';
+import { environment } from 'src/environments/environment';
+import { WebStorageService, LOCAL_STORAGE } from 'ngx-webstorage-service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { LoginComponent } from '../components/login/login.component';
 
 describe('AuthInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      AuthInterceptor
-      ]
-  }));
+  const privateEndpoint = `${environment.apiUrl}categories`;
+  let httpTestingController: HttpTestingController;
+  let http: HttpClient;
+  let storage: WebStorageService;
+
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        AuthInterceptor,
+        { provide: WebStorageService, useExisting: LOCAL_STORAGE },
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: AuthInterceptor,
+          multi: true,
+        },
+      ],
+      imports: [
+        RouterTestingModule.withRoutes([
+          { path: 'login', component: LoginComponent }
+        ]),
+        HttpClientTestingModule,
+      ],
+    })
+    httpTestingController = TestBed.inject(HttpTestingController);
+    storage = TestBed.inject(WebStorageService);
+    http = TestBed.inject(HttpClient);
+  });
 
   it('should be created', () => {
     const interceptor: AuthInterceptor = TestBed.inject(AuthInterceptor);
     expect(interceptor).toBeTruthy();
   });
+
+
+  it('should not have an Authorization header if token not in storage', fakeAsync(() => {
+    storage.remove('token');
+
+    http.get(privateEndpoint).subscribe();
+
+    const req = httpTestingController.expectOne(privateEndpoint);
+    expect(req.request.headers.has('Authorization')).toEqual(false);
+  }))
+
+  it('should have an Authorization header if token in storage', fakeAsync(() => {
+    storage.set('token', 'mytoken');
+
+    http.get(privateEndpoint).subscribe();
+
+    const req = httpTestingController.expectOne(privateEndpoint);
+    expect(req.request.headers.has('Authorization')).toEqual(true);
+  }))
+
+  it('should have an Authorization header equal to Bearer + storage token', fakeAsync(() => {
+    storage.set('token', 'mytoken');
+
+    http.get(privateEndpoint).subscribe();
+
+    const req = httpTestingController.expectOne(privateEndpoint);
+    expect(req.request.headers.get('Authorization')).toBe('Bearer mytoken');
+  }))
+
+
+
 });
