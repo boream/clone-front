241c9c7ac8398699129679091d886f8b4d36e836
Claudia <claudiabdm@hotmail.es>
2020-05-26 11:48:36 +0200

    Add user routing

--- src/app/features/user/user-routing.module.ts
+++ src/app/features/user/user-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './user.component';
+import { SettingsComponent } from './settings/settings.component';
 
 
 const routes: Routes = [
-  {path: '/@:username', component: UserComponent}
+  {path: '/@:username', component: UserComponent},
+  {path: '/settings', component: SettingsComponent},
 ];
 
 @NgModule({
--- src/app/features/user/user.module.ts
+++ src/app/features/user/user.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
+import { SettingsComponent } from './settings/settings.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
-  declarations: [UserComponent],
+  declarations: [UserComponent, SettingsComponent, HeaderComponent],
   imports: [
     CommonModule,
     UserRoutingModule
