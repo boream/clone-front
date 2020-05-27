feace51e43b56a0362a8198e92f7ad043acaa621
Claudia <claudiabdm@hotmail.es>
2020-05-27 14:55:09 +0200

    Add styles and logic to change-password component

--- src/app/features/user/components/change-password/change-password.component.ts
+++ src/app/features/user/components/change-password/change-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-change-password',
@@ -6,10 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./change-password.component.scss']
 })
 export class ChangePasswordComponent implements OnInit {
+  formChangePswd: FormGroup;
 
-  constructor() { }
+  emailPattern: any =
+  /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
 
-  ngOnInit(): void {
+  charactersPattern: any = /A-Za-z0-9\-\_]+/;
+
+  error: boolean;
+
+  constructor(private fb: FormBuilder, private auth: AuthService) {
+
   }
 
+  ngOnInit(): void {
+
+    this.formChangePswd = this.fb.group({
+      currentPassword: ['', [Validators.required, Validators.minLength(6)]],
+      newPassword: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  submit(form) {
+    this.auth.signup(form.value).subscribe((response) => {
+
+    },(error) => {
+      this.error = error.error.message[0].messages[0].message;
+    });
+  }
+
+  passwordHasError(password) {
+    if (password.touched ||  password.dirty)  {
+      return password.errors?.minlength || password.errors?.required;
+    }
+  }
+
+  errorClose() {
+    this.error= null;
+  }
 }
