f84b64e516713eb04947498597da2051a3d2df66
david <deivid.develop@gmail>
2020-05-27 15:47:58 +0200

    Creating the grid to display the images

--- src/app/features/user/components/user/user.component.ts
+++ src/app/features/user/components/user/user.component.ts
@@ -20,16 +20,6 @@ export class UserComponent implements OnInit {
   userName: string = '';
 
   ngOnInit(): void {
-    this.model3dService.getModels().subscribe((result) => {
-      debugger
-      //Ordena el resultado de los mas votados de mayor a menor y coge los 8 primeros;
-      this.featuredImages = result.sort((a, b) => b.likes - a.likes).slice(0, 4);
-      //Ordena el resultado de los ultimos 8 que se han subido a la base de datos;
-      this.newModels = result.sort((a, b)  => b.createDate - a.createDate).slice(0, 15);
-      //
-      // this.fanArtModels = result.map(x => x._id == "5e823f50cbca55661229c388");
-    });
-
   }
 
 }
