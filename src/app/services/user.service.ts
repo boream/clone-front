af841918ff4cd6c32b617deae693d13c0a4e4098
Claudia <claudiabdm@hotmail.es>
2020-05-28 19:39:34 +0200

    Fix bugs and refactor methods

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, pipe, forkJoin } from 'rxjs';
+import { Observable, pipe, forkJoin, of } from 'rxjs';
 import { User } from '../types/user';
 import { map } from 'rxjs/internal/operators/map';
 import { environment } from 'src/environments/environment';
-import { switchMap, tap, scan } from 'rxjs/operators';
+import { switchMap, tap, scan, filter } from 'rxjs/operators';
 import { Router } from '@angular/router';
 
 @Injectable({
@@ -24,13 +24,16 @@ export class UserService {
     return this.http.get<User>(`${this.userUrl}/me`);
   }
 
-  getLoggedUserImages(): Observable<User['images']> {
+  getLoggedUserImages(): Observable<User['images']> | Observable<any[]> {
     return this.getLoggedUser()
       .pipe(
         map((user: User) => user.images.map(img => `id_in=${img}`)),
-        switchMap((images: string[]) => {
-          const query = images.join('&');
-          return this.getImage(query);
+        switchMap((images) => {
+          if(images.length > 0) {
+            const query = images.join('&');
+            return this.getImage(query);
+          }
+          return of([]);
         }),
       )
   }
@@ -46,27 +49,20 @@ export class UserService {
     return this.http.get<User['images']>(`${this.imagesUrl}?${query}`);
   }
 
-  updateUser(userId: string, user: User) {
-    return this.http.put<User>(`${this.userUrl}/${userId}`, user);
+  updateUser(user: User) {
+    return this.http.put<User>(`${this.userUrl}/${user.id}`, user)
   }
 
-  changePassword(password: string) {
-    return this.getLoggedUser()
+  changePassword(user: User, password: string) {
+    user['password'] = password;
+    return this.updateUser(user);
+  }
+
+  closeAccount(user: User): Observable<Object> {
+    return this.http.delete(`${this.userUrl}/${user.id}`)
       .pipe(
-        switchMap((user: User) => {
-          user['password'] = password;
-          return this.updateUser(user.id, user);
-        })
+        tap(() => this.router.navigate(['/login']))
       )
-  }
-
-  closeAccount(): Observable<Object> {
-    return this.getLoggedUser().pipe(
-      switchMap((user: User) => {
-        return this.http.delete(`${this.userUrl}/${user.id}`)
-      }),
-      tap(() => this.router.navigate(['/login']))
-    )
   }
 
   getUserByUsername(username: string): Observable<User> {
@@ -77,9 +73,9 @@ export class UserService {
   }
 
   getUserImages(username: string): Observable<User['images']> {
-    return this.getUserByUsername(username)
+    return this.http.get(`${this.userUrl}/?username=${username}`)
       .pipe(
-        map((user: User) => user.images),
+        map((user: User) => user[0].images),
       )
   }
 
