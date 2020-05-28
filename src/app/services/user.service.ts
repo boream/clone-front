9038af05dafb09ffbb0e66f5a091973f618ae803
Claudia <claudiabdm@hotmail.es>
2020-05-28 12:24:49 +0200

    Create getUserimages() method

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -4,6 +4,7 @@ import { Observable, pipe } from 'rxjs';
 import { User } from '../types/user';
 import { map } from 'rxjs/internal/operators/map';
 import { environment } from 'src/environments/environment';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,7 @@ import { environment } from 'src/environments/environment';
 export class UserService {
 
   userUrl = `${environment.apiUrl}users`;
+  imagesUrl = `${environment.apiUrl}images`;
 
   constructor(
     private http: HttpClient
@@ -18,6 +20,10 @@ export class UserService {
 
   getUser(): Observable<User> {
     return this.http.get<User>(this.userUrl);
+  }
+
+  getUserImages(): Observable<User['images']> {
+    return this.http.get<User>(this.userUrl)
   }
 
   getUserAvatar(): Observable<string> {
