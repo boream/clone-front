f6d717568fb518dc66d25d88d07961de7f8f773a
alexseik <alexseik@hotmail.com>
2020-05-31 20:30:31 +0200

    Fix image service and image type

--- src/app/types/image.ts
+++ src/app/types/image.ts
@@ -3,13 +3,13 @@ import { Tag } from './tag';
 import { User } from './user';
 
 export interface Image {
-    id: string,
+    id?: string,
     file: File;
     published: Boolean;
     title?: String;
     category?: Category;
     tags?: Tag[];
     publishedDate?: Date;
-    url: string;
+    url?: string;
     user?: User;
 }
