1d9d4369fa365ea43f8ca45ebb39c10b2387d19e
alexseik <alexseik@hotmail.com>
2020-06-01 08:12:21 +0200

    Fix close-account component test

--- src/app/features/user/components/close-account/close-account.component.spec.ts
+++ src/app/features/user/components/close-account/close-account.component.spec.ts
@@ -1,19 +1,35 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CloseAccountComponent } from './close-account.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { asyncData } from 'src/test-utils';
 
 describe('CloseAccountComponent', () => {
   let component: CloseAccountComponent;
   let fixture: ComponentFixture<CloseAccountComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CloseAccountComponent ]
-    })
-    .compileComponents();
-  }));
+  let userServiceSpy: { getLoggedUser: jasmine.Spy };
 
   beforeEach(() => {
+    userServiceSpy = jasmine.createSpyObj('UserService', ['getLoggedUser']);
+    TestBed.configureTestingModule({
+      declarations: [ CloseAccountComponent ],
+      providers:[
+        { provide: UserService, useValue: userServiceSpy }
+      ],
+      imports: [ 
+        ReactiveFormsModule,
+        RouterTestingModule
+       ]
+    });
+
+    userServiceSpy.getLoggedUser.and.returnValue(asyncData({
+      id: '1',
+      username: 'uno',
+      email: 'uno@email.com'
+    }));
+
     fixture = TestBed.createComponent(CloseAccountComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
