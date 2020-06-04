f4ca6ae791e8add9b03c7ae763ad2894a028b52f
david <deivid.develop@gmail>
2020-06-04 12:15:07 +0200

    Create sub-header component

--- src/app/features/page-img/page-img.module.ts
+++ src/app/features/page-img/page-img.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SmartImgComponent } from './components/smart-img/smart-img.component';
+import { SubHeaderComponent } from './components/sub-header/sub-header.component';
 
 
 
 @NgModule({
-  declarations: [SmartImgComponent],
+  declarations: [SmartImgComponent, SubHeaderComponent],
   imports: [
     CommonModule
   ]
